1afacadeb81ddda1534fbb0ddd8b09cdafee61bf
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-04-02 19:30:50 +0200

    Added font awesome icons to navbar

--- src/app/components/navigation/navbar/navbar.component.ts
+++ src/app/components/navigation/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faHouse, faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  faHouse = faHouse
+  faMagnifyingGlass = faMagnifyingGlass
+  faPlus = faPlus
 
   constructor() { }
 
