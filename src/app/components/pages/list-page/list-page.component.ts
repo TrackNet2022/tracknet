246d63a352790b444d2e25cf47fe99a90429930c
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-08 20:37:28 +0200

    Added demo calendar

--- src/app/components/pages/list-page/list-page.component.ts
+++ src/app/components/pages/list-page/list-page.component.ts
@@ -1,12 +1,70 @@
 import { Component, OnInit } from '@angular/core'
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView
+} from 'angular-calendar'
+import { addDays, addHours, endOfMonth, startOfDay, subDays } from 'date-fns'
 
 @Component({
   selector: 'list-page',
   templateUrl: './list-page.component.html',
   styleUrls: ['./list-page.component.scss']
 })
-export class ListPageComponent implements OnInit {
-  constructor() {}
+export class ListPageComponent {
+  viewDate: Date = new Date()
 
-  ngOnInit(): void {}
+  colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3'
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF'
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA'
+    }
+  }
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: this.colors.red,
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: this.colors.yellow
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: this.colors.blue,
+      allDay: true
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+
+      end: addHours(new Date(), 2),
+      title: 'A draggable and resizable event',
+      color: this.colors.yellow,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    }
+  ]
 }
