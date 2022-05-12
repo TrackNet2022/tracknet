20bff82a020814ec4415db3731c3f8b624a6da66
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-12 20:49:11 +0200

    Working calendar

--- src/app/components/elements/calendar-element/calendar-element.component.ts
+++ src/app/components/elements/calendar-element/calendar-element.component.ts
@@ -1,22 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core'
-import { CalendarEvent } from 'angular-calendar'
-import { addDays, addHours, endOfMonth, startOfDay, subDays } from 'date-fns'
-import { Subject } from 'rxjs/internal/Subject'
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core'
 import { Serie } from 'src/app/models/serie'
-import { MoviedbService } from 'src/app/services/moviedb.service'
+
+import { CalendarEvent, CalendarView } from 'angular-calendar'
+import {
+  isSameMonth,
+  isSameDay,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  startOfDay,
+  endOfDay
+} from 'date-fns'
+import { combineLatest, map, Observable } from 'rxjs'
 import { SerieService } from 'src/app/services/serie.service'
+import { LocalStorageService } from 'src/app/services/local-storage.service'
 
-//New
-// function getTimezoneOffsetString(date: Date): string {
-//   const timezoneOffset = date.getTimezoneOffset()
-//   const hoursOffset = String(
-//     Math.floor(Math.abs(timezoneOffset / 60))
-//   ).padStart(2, '0')
-//   const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0')
-//   const direction = timezoneOffset > 0 ? '-' : '+'
+function getTimezoneOffsetString(date: Date): string {
+  const timezoneOffset = date.getTimezoneOffset()
+  const hoursOffset = String(
+    Math.floor(Math.abs(timezoneOffset / 60))
+  ).padStart(2, '0')
+  const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0')
+  const direction = timezoneOffset > 0 ? '-' : '+'
 
-//   return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`
-// }
+  return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`
+}
 
 @Component({
   selector: 'calendar-element',
@@ -24,87 +33,77 @@ import { SerieService } from 'src/app/services/serie.service'
   templateUrl: './calendar-element.component.html',
   styleUrls: ['./calendar-element.component.scss']
 })
-export class CalendarElementComponent {
-  newEpisodesThisMonth: Serie[] = []
-  _SerieService: SerieService
-  refresh = new Subject<void>()
-  events: CalendarEvent[] = []
-
-  constructor(_SerieService: SerieService) {
-    this._SerieService = _SerieService
-  }
-  // constructor(_MoviedbService: MoviedbService, _SerieService: SerieService) {
-  //   _MoviedbService.getSerieWithChapterThisMonth().subscribe({
-  //     next: (e: any) => {
-  //       const results = e.results
-  //       results.forEach((result: Serie) => {
-  //         _SerieService.getSerieDetail(result.id).subscribe({
-  //           next: (e: any) => {
-  //             result = e.data
-  //             const { name, next_episode_to_air, id } = result
-  //             const air_date = next_episode_to_air?.air_date
-  //               ? next_episode_to_air?.air_date
-  //               : new Date().toString()
-  //             this.events.push({
-  //               start: new Date(Date.parse(air_date)),
-  //               end: new Date(Date.parse(air_date)),
-  //               title: name,
-  //               color: this.colors.red,
-  //               allDay: true
-  //             })
-  //             console.log({ event: this.events })
-  //           }
-  //         })
-  //       })
-  //     },
-  //     complete: () => {
-  //       console.log(this.events)
-  //     },
-  //     error: () => console.log(this.newEpisodesThisMonth)
-  //   })
-  //   this._SerieService = _SerieService
-  // }
+export class CalendarElementComponent implements OnInit {
+  view: CalendarView = CalendarView.Month
 
   viewDate: Date = new Date()
 
-  colors: any = {
-    red: {
-      primary: '#ad2121',
-      secondary: '#FAE3E3'
-    },
-    blue: {
-      primary: '#1e90ff',
-      secondary: '#D1E8FF'
-    },
-    yellow: {
-      primary: '#e3bc08',
-      secondary: '#FDF1BA'
+  activeDayIsOpen = false
+  events$: Observable<CalendarEvent<{ film: Serie }>[]> = new Observable()
+  constructor(
+    private _serieService: SerieService,
+    private _localStorage: LocalStorageService
+  ) {
+    this.fetchEvents()
+  }
+  ngOnInit(): void {
+    this.fetchEvents()
+  }
+
+  fetchEvents(): void {
+    const getStart: any = {
+      month: startOfMonth,
+      week: startOfWeek,
+      day: startOfDay
+    }[this.view]
+
+    const getEnd: any = {
+      month: endOfMonth,
+      week: endOfWeek,
+      day: endOfDay
+    }[this.view]
+
+    const userList = this._localStorage.getItem('data')
+    if (userList.length > 1) {
+      const requestList = userList.map((id: number) => {
+        return this._serieService
+          .getSerieDetail(id)
+          .pipe(map((response) => response.data))
+      })
+      this.events$ = combineLatest<CalendarEvent[]>(requestList).pipe(
+        map((series: any) => {
+          return series.map((serie: Serie) => {
+            return {
+              title: serie.name,
+              start: new Date(),
+              allDay: true,
+              meta: {
+                serie
+              }
+            }
+          })
+        })
+      )
     }
   }
 
-  loadSeries() {
-    this.newEpisodesThisMonth.forEach((serie: Serie) => {
-      this._SerieService.getSerieDetail(serie.id).subscribe({
-        next: (e: any) => {
-          serie = e.data
-        },
-        complete: () => {
-          const { name, next_episode_to_air, id } = serie
-          const air_date = next_episode_to_air?.air_date
-            ? next_episode_to_air?.air_date
-            : new Date().toString()
-          // console.log(new Date(Date.parse(air_date)))
-          this.events.push({
-            start: new Date(Date.parse(air_date)),
-            // end: new Date(Date.parse(air_date)),
-            title: name,
-            color: this.colors.red,
-            allDay: true
-          })
-          this.refresh.next()
-          // console.log(this.events)
-        }
-      })
-    })
+  dayClicked({
+    date,
+    events
+  }: {
+    date: Date
+    events: CalendarEvent<{ serie: Serie }>[]
+  }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false
+      } else {
+        this.activeDayIsOpen = true
+        this.viewDate = date
+      }
+    }
   }
 }
