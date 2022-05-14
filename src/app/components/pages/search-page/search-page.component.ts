66b957431dd26217c5b6a52a14b11fbf7a6b87f6
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-14 12:46:01 +0200

    Refactor with new api

--- src/app/components/pages/search-page/search-page.component.ts
+++ src/app/components/pages/search-page/search-page.component.ts
@@ -1,37 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
 import { Serie } from 'src/app/models/serie'
 import { MoviedbService } from 'src/app/services/moviedb.service'
+import { SerieService } from 'src/app/services/serie.service'
 @Component({
   selector: 'search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent {
-  resultSeries: Serie[] = []
+  resultSeries: Observable<Serie[]> = new Observable()
   searchTerm = ''
-  _mobiedb: MoviedbService
 
-  constructor(_mobiedb: MoviedbService) {
-    this._mobiedb = _mobiedb
-  }
+  constructor(private _serieService: SerieService) { }
 
   onChange(searchTerm: string) {
     if (searchTerm.length > 3) {
-      this.resultSeries = this.search()
+      this.search()
     }
   }
 
   search() {
-    const searchedSeries: Serie[] = []
     console.log(this.searchTerm)
-    this._mobiedb.searchSerieByName(this.searchTerm).subscribe({
-      next: (e: any) => {
-        const results = e.results
-        console.log(results)
-        results.forEach((result: Serie) => searchedSeries.push(result))
-      }
-    })
-    return searchedSeries
+    this.resultSeries = this._serieService.searchByTerm(this.searchTerm).pipe()
   }
 }
