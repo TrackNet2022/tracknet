f685bdeed051d4f150d3800d372ecf78949ab8f9
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-15 21:50:58 +0200

    My list upgrade

--- src/app/components/pages/list-page/list-page.component.ts
+++ src/app/components/pages/list-page/list-page.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit } from '@angular/core'
+import { map, Observable } from 'rxjs'
+import { Serie } from 'src/app/models/serie'
 
+import { UserService } from 'src/app/services/user.service'
+import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'list-page',
   templateUrl: './list-page.component.html',
   styleUrls: ['./list-page.component.scss']
 })
-export class ListPageComponent {}
+export class ListPageComponent implements OnInit {
+  userSerieList$: Observable<Serie[]> = new Observable()
+  userNextEpisodes$: Observable<Serie[]> = new Observable()
+  faCalendar = faCalendar
+  actualView = 'list'
+
+  constructor(private _userService: UserService) {}
+
+  ngOnInit(): void {
+    const data = this._userService.getUserSerieList()
+    if (data != null) {
+      this.userSerieList$ = data
+      this.userNextEpisodes$ = data.pipe(
+        map((series: Serie[]) =>
+          series.filter((serie: Serie) => serie.next_episode_to_air)
+        )
+      )
+    }
+  }
+
+  onClick() {
+    if (this.actualView == 'calendar') this.actualView = 'list'
+    else this.actualView = 'calendar'
+  }
+}
