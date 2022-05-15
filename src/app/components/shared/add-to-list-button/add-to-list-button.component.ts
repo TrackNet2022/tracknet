8cd30417ae1c2629a26c23ade92a79eed1556ce1
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-15 12:25:57 +0200

    Fix of button

---
+++ src/app/components/shared/add-to-list-button/add-to-list-button.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { LocalStorageService } from 'src/app/services/local-storage.service'
+import { faBookmark, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+
+
+@Component({
+  selector: 'add-to-list-button',
+  templateUrl: './add-to-list-button.component.html',
+  styleUrls: ['./add-to-list-button.component.scss']
+})
+export class AddToListButtonComponent implements OnInit {
+  @Input() icon: IconDefinition = faBookmark
+  @Input() serieId = 0
+
+  status = false
+  constructor(private _localStorage: LocalStorageService) {}
+  ngOnInit(): void {
+    const list: number[] = this._localStorage.getItem('data')
+    if (list && list.includes(this.serieId)) {
+      this.status = true
+    }
+  }
+
+  saveToList(id: number) {
+    const currentData: number[] = this._localStorage.getItem('data')
+    console.log(currentData)
+    if (currentData && !currentData.includes(id)) {
+      currentData.push(id)
+      this._localStorage.setItem('data', currentData)
+    } else if (currentData && currentData.includes(id)) {
+      this._localStorage.setItem(
+        'data',
+        currentData.filter((e) => e != id)
+      )
+    } else {
+      this._localStorage.setItem('data', [id])
+    }
+    this.status = !this.status
+  }
+}
